9682669a128df2875d6e5eb35ce5c9aef52fe775
mjainamshah <jainam.shah@student.moringaschool.com>
2023-12-05 13:03:33 +0300

    COMPLETED

---
+++ app/migrations/versions/c125e3e33727_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: c125e3e33727
+Revises: 
+Create Date: 2023-12-05 12:26:24.607304
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c125e3e33727'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
